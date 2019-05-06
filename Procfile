7a317eb2d34879747e370a4609107ebc6ed4e334
elcior <elciorfscarvalho@gmail.com>
2019-05-06 01:13:25 -0300

    Configurando o Heroku

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn projeto_topicos.wsgi --log-file -
+
--- projeto_topicos/settings.py
+++ projeto_topicos/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from decouple import config
+from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rz*=_b9ir3k^abxrcp$mhry5o7^o!5cy_hq^+ue+@16%&*shb%'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['projeto-topicos.herokuapp.com']
 
 
 # Application definition
@@ -73,12 +75,11 @@ WSGI_APPLICATION = 'projeto_topicos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3') 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+    'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
+} 
 
 
 # Password validation
@@ -118,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
