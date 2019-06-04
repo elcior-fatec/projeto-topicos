c0b3ef0bbfbd73c6469ab0c82a2197c9e4262b98
elcior.carvalho <elcior.carvalho@comlink.com.br>
2019-06-04 12:11:58 -0300

    Filtro completo. De Secoes ate Classes

--- filtro_cnae/urls.py
+++ filtro_cnae/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import list_secoes, list_divisoes
+from .views import list_secoes, list_divisoes, list_grupos, list_classes
 
 
 urlpatterns = [
     path('list-secoes/', list_secoes, name="list_secoes"),
     path('list-divisoes/', list_divisoes, name="list_divisoes"),
+    path('list-grupos/', list_grupos, name="list_grupos"),
+    path('list-classes/', list_classes, name="list_classes"),
 ]
--- filtro_cnae/views.py
+++ filtro_cnae/views.py
@@ -8,13 +8,9 @@ import requests
 import os
 
 
-# TODO funções para realizar a busca pela classe CNAE
-
-def get_classes_json():
-    classes_json = requests.get(url='https://servicodados.ibge.gov.br/api/v2/cnae/classes')
-    return classes_json
-
-
+'''
+Funções para captura dos json
+'''
 def get_divisoes_json(secao_id):
     url = f'https://servicodados.ibge.gov.br/api/v2/cnae/secoes/{secao_id}/divisoes'
     divisoes_json = requests.get(url)
@@ -22,6 +18,23 @@ def get_divisoes_json(secao_id):
     return divisoes_collected
 
 
+def get_grupos_json(divisoes_id):
+    url = f'https://servicodados.ibge.gov.br/api/v2/cnae/divisoes/{divisoes_id}/grupos'
+    grupos_json = requests.get(url)
+    grupos_collected = grupos_json.json()
+    return grupos_collected
+
+
+def get_classes_json(grupos_id):
+    url = f'https://servicodados.ibge.gov.br/api/v2/cnae/grupos/{grupos_id}/classes'
+    classes_json = requests.get(url)
+    classes_collected = classes_json.json()
+    return classes_collected
+
+
+'''
+Funções para processamento das requests
+'''
 @login_required
 def list_secoes(request):
     form = Secoes()
@@ -33,5 +46,38 @@ def list_divisoes(request):
     divisoes_collected = get_divisoes_json(request.POST.get('secoes'))
     divisoes = []
     for divisao in divisoes_collected:
-        divisoes.append({'id': f"{divisao['id']}", 'discricao': f"{divisao['descricao']}"})
+        divisoes.append(
+            {
+                'id': f"{divisao['id']}",
+                'descricao': f"{divisao['descricao']}"
+            }
+        )
     return render(request, 'list-divisoes.html', {'divisoes': divisoes})
+
+
+@login_required
+def list_grupos(request):
+    grupos_collected = get_grupos_json(request.POST.get('divisao'))
+    grupos = []
+    for grupo in grupos_collected:
+        grupos.append(
+            {
+                'id': f"{grupo['id']}",
+                'descricao': f"{grupo['descricao']}"
+            }
+        )
+    return render(request, 'list-grupos.html', {'grupos': grupos})
+
+
+def list_classes(request):
+    classes_collected = get_classes_json(request.POST.get('grupo'))
+    classes = []
+    for classe in classes_collected:
+        classes.append(
+            {
+                'id': f"{classe['id']}",
+                'descricao': f"{classe['descricao']}",
+                'observacoes': classe['observacoes'],
+            }
+        )
+    return render(request, 'list-classes.html', {'classes': classes})
