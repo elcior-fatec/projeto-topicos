0115c0e3d6e38eeaafce5b5eb669d290a1b070bf
elcior.carvalho <elcior.carvalho@comlink.com.br>
2019-09-17 11:27:02 -0300

    Criando branch 'teste-rest'

--- filtro_cnae/admin.py
+++ filtro_cnae/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from .models import SearchedCNAE
 
 
-admin.site.register(SearchedCNAE)
+# admin.site.register(SearchedCNAE)
--- ibge_cnae/admin.py
+++ ibge_cnae/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import IbgeCNAE
 
-# Register your models here.
+
+admin.site.register(IbgeCNAE)
